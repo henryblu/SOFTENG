21d025366360f34f7af0613325b5958e85b6e806
henry <henry.s.blue@gmail.com>
2025-11-14 14:42:10 +0100

    Project deliverable 1

--- VehicleTaxes/src/Vehicle.java
+++ VehicleTaxes/src/Vehicle.java
@@ -3,7 +3,7 @@ import java.util.Objects;
 
 public abstract class Vehicle {
     public String plate, maker, model;
-    public Person owner; // nullable
+    public Person owner;
 
     protected Vehicle(String plate, String maker, String model) {
         this.plate = Objects.requireNonNull(plate).trim();
